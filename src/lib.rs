fa8e30b658e858013a348e44a460a994d169a9e4
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-09-14 00:33:51 +0900

    Test speed of Lorenz 63 model (classical RK4)

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+
+extern crate ndarray;
+extern crate num;
+
+use ndarray::prelude::*;
+
+#[inline(always)]
+pub fn lorenz63(p: f64, b: f64, r: f64, mut v: Array<f64, Ix>) -> Array<f64, Ix> {
+    let x = v[0];
+    let y = v[1];
+    let z = v[2];
+    v[0] = p * (y - x);
+    v[1] = x * (r - z) - y;
+    v[2] = x * y - b * z;
+    v
+}
+
+pub fn euler<TEO, D: Dimension>(u: &TEO, dt: f64, x: Array<f64, D>) -> Array<f64, D>
+    where TEO: Fn(Array<f64, D>) -> Array<f64, D>
+{
+    let y = x.clone();
+    x + dt * u(y)
+}
+
+pub fn rk4<TEO, D: Dimension>(u: &TEO, dt: f64, x: Array<f64, D>) -> Array<f64, D>
+    where TEO: Fn(Array<f64, D>) -> Array<f64, D>
+{
+    let mut l = x.clone();
+    l = u(l);
+    let k1 = l.clone();
+    l = (0.5 * dt) * l + &x;
+    l = u(l);
+    let k2 = l.clone();
+    l = (0.5 * dt) * l + &x;
+    l = u(l);
+    let k3 = l.clone();
+    l = dt * l + &x;
+    l = u(l);
+    x + dt * (k1 + 2.0 * (k2 + k3) + l) / 6.0
+}
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+
+extern crate ndarray;
+extern crate ndarray_odeint;
+
+use ndarray::prelude::*;
+
+fn main() {
+    let mut x = arr1(&[1.0, 0.0, 0.0]);
+    let l = |y| ndarray_odeint::lorenz63(10., 28., 8.0 / 3.0, y);
+    let teo = |y| ndarray_odeint::rk4(&l, 0.01, y);
+    for _ in 0..100000000 {
+        x = teo(x);
+    }
+    println!("{:?}", x);
+}
