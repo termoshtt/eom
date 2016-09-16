3f81adb5d06c78ef881178cb44e4d1e2d63936df
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-09-16 21:51:18 +0900

    Reduce time steps

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let mut x = arr1(&[1.0, 0.0, 0.0]);
     let l = |y| ndarray_odeint::lorenz63(10., 28., 8.0 / 3.0, y);
     let teo = |y| ndarray_odeint::rk4(&l, 0.01, y);
-    for _ in 0..100000000 {
+    for _ in 0..10000000 {
         x = teo(x);
     }
     println!("{:?}", x);
