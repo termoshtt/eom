da660b7aaa2c6c314b0606c777e5464fa4dfa410
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-11-15 16:52:10 +0900

    Output time

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,14 +5,15 @@ extern crate ndarray_odeint;
 use ndarray::prelude::*;
 
 fn main() {
+    let dt = 0.01;
     let l = |y| ndarray_odeint::lorenz63(10.0, 28.0, 8.0 / 3.0, y);
     let ts = ndarray_odeint::TimeSeries {
-        teo: |y| ndarray_odeint::rk4(&l, 0.01, y),
+        teo: |y| ndarray_odeint::rk4(&l, dt, y),
         state: arr1(&[1.0, 0.0, 0.0]),
     };
     let end_time = 10000;
-    println!("x,y,z");
-    for v in ts.take(end_time) {
-        println!("{},{},{}", v[0], v[1], v[2]);
+    println!("time,x,y,z");
+    for (t, v) in ts.take(end_time).enumerate() {
+        println!("{},{},{},{}", t as f64 * dt, v[0], v[1], v[2]);
     }
 }
