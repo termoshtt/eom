d883a1e528ab20329c2e163798e6aa6a4ad413e2
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-01-08 17:03:01 +0900

    Update test

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -12,8 +12,8 @@ use ndarray_odeint::traits::*;
 fn main() {
     let dt = 0.01;
     let eom = l63::Lorenz63::default();
-    let euler = explicit::euler(eom, dt);
-    let ts = iterate(rcarr1(&[1.0, 0.0, 0.0]), |y| euler.iterate(y.clone()));
+    let teo = explicit::rk4(eom, dt);
+    let ts = iterate(rcarr1(&[1.0, 0.0, 0.0]), |y| teo.iterate(y.clone()));
     let end_time = 10000;
     println!("time,x,y,z");
     for (t, v) in ts.take(end_time).enumerate() {
