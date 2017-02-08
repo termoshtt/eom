a1d54bbf2d4a387b2dcdc6f0786cf5c892c52d4b
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-02-08 16:48:37 +0900

    Calc CLV (only exp.)

--- src/bin/clv.rs
+++ src/bin/clv.rs
@@ -15,16 +15,31 @@ fn main() {
     let eom = Lorenz63::default();
     let teo = explicit::rk4(eom, dt);
     let ts = iterate(rcarr1(&[1.0, 0.0, 0.0]), |y| teo.iterate(y.clone()));
-    let qr_series = ts.scan(Array::eye(3), |st, x| {
-        let j = teo.jacobian(x.clone(), 1e-7);
-        let (q_next, r) = j.dot(st).qr().unwrap();
-        let q = std::mem::replace(st, q_next);
-        Some((q, r))
-    });
     let duration = 100000;
-    let exponents = qr_series.map(|(_, r)| r.diag().map(|x| x.abs().ln()))
+    let qr_series = ts.scan(Array::eye(3), |st, x| {
+            let j = teo.jacobian(x.clone(), 1e-7);
+            let (q_next, r) = j.dot(st).qr().unwrap();
+            let q = std::mem::replace(st, q_next);
+            Some((q, r))
+        })
         .skip(duration / 10)
         .take(duration)
-        .fold(Array::zeros(3), |x, y| x + y) / (dt * duration as f64);
-    println!("{:?}", exponents);
+        .collect::<Vec<_>>();
+    let clv = qr_series.iter()
+        .rev()
+        .scan(Array::eye(3), |st, &(ref q, ref r)| {
+            let cd = r.solve_upper(&*st).expect("Failed to solve R");
+            let (c, d) = normalize(cd, NormalizeAxis::Column);
+            let v = q.dot(&c);
+            *st = c;
+            Some((v, d))
+        })
+        .collect::<Vec<_>>();
+    let mut l = Array::zeros(3);
+    for (_, d) in clv.into_iter().rev() {
+        let mut f = Array::from_vec(d);
+        f.map_inplace(|x| *x = x.abs().ln());
+        l -= &f;
+    }
+    println!("{:?}", l / (dt * duration as f64));
 }
