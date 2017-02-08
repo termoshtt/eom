4fdafdc3ce6bbfe37dbfe92ef48ebfa4c7a59a18
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-02-08 17:15:32 +0900

    Show tangency of CLVs

--- src/bin/clv.rs
+++ src/bin/clv.rs
@@ -1,9 +1,11 @@
 
+#[macro_use]
 extern crate ndarray;
 extern crate ndarray_odeint;
 extern crate ndarray_linalg;
 extern crate itertools;
 
+use std::io::Write;
 use ndarray::*;
 use ndarray_linalg::prelude::*;
 use ndarray_odeint::prelude::*;
@@ -31,15 +33,22 @@ fn main() {
             let cd = r.solve_upper(&*st).expect("Failed to solve R");
             let (c, d) = normalize(cd, NormalizeAxis::Column);
             let v = q.dot(&c);
+            let f = Array::from_vec(d).mapv_into(|x| 1.0 / x);
             *st = c;
-            Some((v, d))
+            Some((v, f))
         })
         .collect::<Vec<_>>();
     let mut l = Array::zeros(3);
-    for (_, d) in clv.into_iter().rev() {
-        let mut f = Array::from_vec(d);
-        f.map_inplace(|x| *x = x.abs().ln());
-        l -= &f;
+    println!("v0v1, v0v2, v1v2");
+    for (v, f) in clv.into_iter().rev() {
+        let v0 = v.axis_iter(Axis(1)).nth(0).unwrap();
+        let v1 = v.axis_iter(Axis(1)).nth(1).unwrap();
+        let v2 = v.axis_iter(Axis(1)).nth(2).unwrap();
+        println!("{}, {}, {}", v0.dot(&v1), v0.dot(&v2), v1.dot(&v2));
+        l += &f.map(|x| x.abs().ln());
     }
-    println!("{:?}", l / (dt * duration as f64));
+    write!(&mut std::io::stderr(),
+           "exponents = {:?}\n",
+           l / (dt * duration as f64))
+        .unwrap();
 }
