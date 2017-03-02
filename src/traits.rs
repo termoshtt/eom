68c59db6206ff83b675876af3f0171c435343721
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-03-02 19:58:06 +0900

    Revise trait definition

--- src/traits.rs
+++ src/traits.rs
@@ -1,22 +1,29 @@
 
 use ndarray::{RcArray, Dimension};
 
-/// calculate right hand side (rhs) of equation of motion from current state
+/// Equation of motion (EOM)
 pub trait EOM<D: Dimension> {
+    /// calculate right hand side (rhs) of EOM from current state
     fn rhs(&self, RcArray<f64, D>) -> RcArray<f64, D>;
 }
 
-/// Solve stiff equation.
-/// The linear part is assumed to be diagonalized.
-pub trait SemiImplicitDiag<D: Dimension> {
+/// Stiff equation with diagonalized linear part
+pub trait StiffDiag<D: Dimension>: EOM<D> {
     /// Non-Linear part of EOM
     fn nonlinear(&self, RcArray<f64, D>) -> RcArray<f64, D>;
     /// Linear part of EOM (assume to be diagonalized)
-    fn linear_diagonal(&self) -> Array<f64, D>;
+    fn linear_diagonal(&self) -> RcArray<f64, D>;
+
+    fn rhs(&self, x: RcArray<f64, D>) -> RcArray<f64, D> {
+        let nlin = self.nonlinear(x.clone());
+        let a = self.linear_diagonal();
+        nlin + a * x
+    }
 }
 
-/// calculate next step by integrating the equation of motion
+/// Time-evolution operator
 pub trait TimeEvolution<D: Dimension> {
+    /// calculate next step
     fn iterate(&self, RcArray<f64, D>) -> RcArray<f64, D>;
     fn get_dt(&self) -> f64;
 }
