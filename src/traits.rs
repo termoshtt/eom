5bafd249f340db57085eac3d0eaff169e6d07940
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-03-02 19:50:17 +0900

    Add SemiImplicitDiag

--- src/traits.rs
+++ src/traits.rs
@@ -6,6 +6,15 @@ pub trait EOM<D: Dimension> {
     fn rhs(&self, RcArray<f64, D>) -> RcArray<f64, D>;
 }
 
+/// Solve stiff equation.
+/// The linear part is assumed to be diagonalized.
+pub trait SemiImplicitDiag<D: Dimension> {
+    /// Non-Linear part of EOM
+    fn nonlinear(&self, RcArray<f64, D>) -> RcArray<f64, D>;
+    /// Linear part of EOM (assume to be diagonalized)
+    fn linear_diagonal(&self) -> Array<f64, D>;
+}
+
 /// calculate next step by integrating the equation of motion
 pub trait TimeEvolution<D: Dimension> {
     fn iterate(&self, RcArray<f64, D>) -> RcArray<f64, D>;
