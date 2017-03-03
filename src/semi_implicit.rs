8072cb6772ae0a147029049b81fac34245dc4c2d
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-03-03 18:11:21 +0900

    Bug fix

--- src/semi_implicit.rs
+++ src/semi_implicit.rs
@@ -4,17 +4,24 @@ use super::traits::{StiffDiag, TimeEvolution};
 use super::diag::Diagonal;
 use std::marker::PhantomData;
 
-pub struct SemiImplicitDiagRK4<F: StiffDiag<D>, D: Dimension> {
+pub struct DiagRK4<F: StiffDiag<D>, D: Dimension> {
     f: F,
     lin_half: Diagonal<D>,
     dt: f64,
     phantom_dim: PhantomData<D>,
 }
 
-impl<F: StiffDiag<D>, D: Dimension> SemiImplicitDiagRK4<F, D> {
+pub fn diag_rk4<F, D>(f: F, dt: f64) -> DiagRK4<F, D>
+    where F: StiffDiag<D>,
+          D: Dimension
+{
+    DiagRK4::new(f, dt)
+}
+
+impl<F: StiffDiag<D>, D: Dimension> DiagRK4<F, D> {
     pub fn new(f: F, dt: f64) -> Self {
         let lin_half = Diagonal::new(f.linear_diagonal(), dt / 2.0);
-        SemiImplicitDiagRK4 {
+        DiagRK4 {
             f: f,
             lin_half: lin_half,
             dt: dt,
@@ -23,13 +30,13 @@ impl<F: StiffDiag<D>, D: Dimension> SemiImplicitDiagRK4<F, D> {
     }
 }
 
-impl<F: StiffDiag<D>, D: Dimension> TimeEvolution<D> for SemiImplicitDiagRK4<F, D> {
+impl<F: StiffDiag<D>, D: Dimension> TimeEvolution<D> for DiagRK4<F, D> {
     fn iterate(&self, x: RcArray<f64, D>) -> RcArray<f64, D> {
         // constants
         let dt = self.dt;
         let dt_2 = 0.5 * self.dt;
-        let v13 = 1.0 / 3.0;
-        let v16 = 1.0 / 6.0;
+        let dt_3 = self.dt / 3.0;
+        let dt_6 = self.dt / 6.0;
         // operators
         let l = &self.lin_half;
         let f = &self.f;
@@ -42,7 +49,7 @@ impl<F: StiffDiag<D>, D: Dimension> TimeEvolution<D> for SemiImplicitDiagRK4<F, 
         let k3 = f.nonlinear(l2);
         let l3 = l.iterate(lx + dt * &k3);
         let k4 = f.nonlinear(l3);
-        l.iterate(l.iterate(x + v16 * k1) + v13 * (k2 + k3)) + v16 * k4
+        l.iterate(l.iterate(x + dt_6 * k1) + dt_3 * (k2 + k3)) + dt_6 * k4
     }
     fn get_dt(&self) -> f64 {
         self.dt
