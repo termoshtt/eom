2d7093931c5b284069de21e4f6bd8162d037c8ba
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-02 18:53:44 +0900

    Sample executable

---
+++ src/bin/goy_shell.rs
@@ -0,0 +1,24 @@
+
+extern crate ndarray;
+extern crate ndarray_odeint;
+extern crate itertools;
+extern crate simple_complex;
+extern crate num_traits;
+
+use ndarray::rcarr1;
+use itertools::iterate;
+use ndarray_odeint::prelude::*;
+use simple_complex::c64;
+use num_traits::Zero;
+
+fn main() {
+    let dt = 1e-5;
+    let eom = GoyShell::default();
+    let teo = semi_implicit::diag_rk4(eom, dt);
+    let x0 = rcarr1(&vec![c64::zero(); 27]);
+    let ts = iterate(x0, |y| teo.iterate(y.clone()));
+    let end_time = 10000;
+    for (_, v) in ts.take(end_time).enumerate() {
+        println!("{:?}", v);
+    }
+}
