a0fdba20eac4afb3702a0c659cedcc65a94542a3
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-17 19:44:08 +0900

    Merge duplicated code in semi-implicit scheme

--- src/semi_implicit.rs
+++ src/semi_implicit.rs
@@ -39,9 +39,12 @@ impl<A, F, D> DiagRK4<A, F, D>
     }
 }
 
-impl<'a, A, F, D> TimeEvolution<A, D> for &'a DiagRK4<A, F, D>
+macro_rules! impl_time_evolution {
+    ( $($mut_:tt), * ) => {
+
+impl<'a, A, F, D> TimeEvolution<A, D> for &'a $($mut_),* DiagRK4<A, F, D>
     where A: OdeScalar<f64> + Exponential,
-          &'a F: EOM<A, D>,
+          for<'b> &'b $($mut_),* F: EOM<A, D>,
           D: Dimension
 {
     fn iterate(self, x: RcArray<A, D>) -> RcArray<A, D> {
@@ -52,7 +55,7 @@ impl<'a, A, F, D> TimeEvolution<A, D> for &'a DiagRK4<A, F, D>
         let dt_6 = self.dt / 6.0;
         // operators
         let l = &self.lin_half;
-        let f = &self.f;
+        let f = &$($mut_),* self.f;
         // calc
         let k1 = f.rhs(x.clone());
         let l1 = l.iterate(k1.clone() * dt_2 + &x);
@@ -66,29 +69,7 @@ impl<'a, A, F, D> TimeEvolution<A, D> for &'a DiagRK4<A, F, D>
     }
 }
 
-impl<'a, A, F, D> TimeEvolution<A, D> for &'a mut DiagRK4<A, F, D>
-    where A: OdeScalar<f64> + Exponential,
-          for<'b> &'b mut F: EOM<A, D>,
-          D: Dimension
-{
-    fn iterate(self, x: RcArray<A, D>) -> RcArray<A, D> {
-        // constants
-        let dt = self.dt;
-        let dt_2 = 0.5 * self.dt;
-        let dt_3 = self.dt / 3.0;
-        let dt_6 = self.dt / 6.0;
-        // operators
-        let l = &self.lin_half;
-        let f = &mut self.f;
-        // calc
-        let k1 = f.rhs(x.clone());
-        let l1 = l.iterate(k1.clone() * dt_2 + &x);
-        let k2 = f.rhs(l1);
-        let lx = l.iterate(x.clone());
-        let l2 = k2.clone() * dt_2 + &lx;
-        let k3 = f.rhs(l2);
-        let l3 = l.iterate(lx + &k3 * dt);
-        let k4 = f.rhs(l3);
-        l.iterate(l.iterate(x + k1 * dt_6) + (k2 + k3) * dt_3) + k4 * dt_6
-    }
-}
+}} // impl_time_evolution!
+
+impl_time_evolution!();
+impl_time_evolution!(mut);
