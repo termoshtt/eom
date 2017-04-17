f816a8915589087485c5e2270512bbf061a90867
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-17 18:11:38 +0900

    Update tests

--- tests/lyapunov.rs
+++ tests/lyapunov.rs
@@ -16,7 +16,7 @@ fn jacobian_linearity() {
     let eom = Lorenz63::default();
     let teo = explicit::rk4(eom, 0.01);
     let x0 = rcarr1(&[1.0, 0.0, 0.0]);
-    let j = teo.jacobian(x0, 1e-7);
+    let j = jacobian(&teo, x0, 1e-7);
     let v = Array1::real_normal_init(3, 1.0, 1.0);
     let w = Array1::real_normal_init(3, 1.0, 1.0);
     all_close_l2(&(j.dot(&v) + j.dot(&w)), &j.dot(&(v + w)), 1e-5).unwrap();
@@ -27,7 +27,7 @@ fn jacobian_matrix_shape() {
     let eom = Lorenz63::default();
     let teo = explicit::rk4(eom, 0.01);
     let x0 = rcarr1(&[1.0, 0.0, 0.0]);
-    let j = teo.jacobian(x0, 1e-7);
+    let j = jacobian(&teo, x0, 1e-7);
     let v = Array2::real_normal_init((3, 2), 1.0, 1.0);
     let jv = j.dot(&v);
     assert!(jv.shape() == v.shape());
