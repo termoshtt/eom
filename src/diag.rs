d7b53445a3c43a3a676a8f5af1be2afa3e8336ed
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-05-26 03:49:31 +0900

    impl ArrayViewMut interface for Diagonal

--- src/diag.rs
+++ src/diag.rs
@@ -53,3 +53,15 @@ impl<'a, A, D> TimeEvolution<A, OwnedRcRepr<A>, D> for &'a Diagonal<A, D>
         x
     }
 }
+
+impl<'a, A, D> TimeEvolution<A, ViewRepr<&'a mut A>, D> for &'a Diagonal<A, D>
+    where A: OdeScalar<f64> + Exponential,
+          D: Dimension
+{
+    fn iterate(self, mut x: ArrayViewMut<A, D>) -> ArrayViewMut<A, D> {
+        for (val, d) in x.iter_mut().zip(self.diag.iter()) {
+            *val = *val * *d;
+        }
+        x
+    }
+}
