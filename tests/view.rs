7b7349521de60f9ac0940c18e3dd586de1b5d410
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-05-27 03:19:44 +0900

    Update tests

--- tests/view.rs
+++ tests/view.rs
@@ -3,12 +3,12 @@ extern crate ndarray;
 extern crate ndarray_odeint;
 
 use ndarray::*;
-use ndarray_odeint::prelude::*;
+use ndarray_odeint::*;
 
 #[test]
 fn view_mut() {
     let dt = 0.01;
-    let eom = Lorenz63::default();
+    let eom = model::Lorenz63::default();
     let teo = explicit::rk4(eom, dt);
     let mut x: Array1<f64> = arr1(&[1.0, 0.0, 0.0]);
     let _x = teo.iterate(x.view_mut());
