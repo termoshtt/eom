7dcd36fe89c66504c31e8dbc039046abb7762eb5
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-05-27 20:05:13 +0900

    Add NonLinear trait

--- src/semi_implicit.rs
+++ src/semi_implicit.rs
@@ -45,7 +45,7 @@ macro_rules! impl_time_evolution {
 impl<'a, A, S, F, D> TimeEvolution<A, S, D> for &'a $($mut_),* DiagRK4<A, F, D>
     where A: OdeScalar<f64> + Exponential,
           S: DataMut<Elem=A> + DataClone<Elem=A>,
-          for<'b> &'b $($mut_),* F: EOM<A, S, D>,
+          for<'b> &'b $($mut_),* F: NonLinear<A, S, D>,
           D: Dimension
 {
     fn iterate(self, x: ArrayBase<S, D>) -> ArrayBase<S, D> {
@@ -60,16 +60,16 @@ impl<'a, A, S, F, D> TimeEvolution<A, S, D> for &'a $($mut_),* DiagRK4<A, F, D>
         // calc
         let mut x_ = x.to_owned();
         let lx = l.iterate(x.clone());
-        let mut k1 = f.rhs(x);
+        let mut k1 = f.nlin(x);
         let k1_ = k1.to_owned();
         azip!(mut k1, x_ in { *k1 = x_ + *k1 * dt_2 });
-        let mut k2 = f.rhs(l.iterate(k1));
+        let mut k2 = f.nlin(l.iterate(k1));
         let k2_ = k2.to_owned();
         azip!(mut k2, lx in { *k2 = lx + *k2 * dt_2 });
-        let mut k3 = f.rhs(k2);
+        let mut k3 = f.nlin(k2);
         let k3_ = k3.to_owned();
         azip!(mut k3, lx in { *k3 = lx + *k3 * dt });
-        let mut k4 = f.rhs(l.iterate(k3));
+        let mut k4 = f.nlin(l.iterate(k3));
         azip!(mut x_, k1_ in { *x_ = *x_ + k1_ * dt_6 });
         let mut x_ = l.iterate(x_);
         azip!(mut x_, k2_, k3_ in { *x_ = *x_ + (k2_ + k3_) * dt_3 });
