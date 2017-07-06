1e00c63f9254de70934aab0567faa42b9e485352
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-07-06 17:53:33 +0900

    Update lyapunov example

--- examples/lyapunov.rs
+++ examples/lyapunov.rs
@@ -1,124 +1,46 @@
-//! Lyapunov Analysis
+//! Lyapunov Analysis example
+
+extern crate ndarray;
+extern crate ndarray_odeint;
+extern crate ndarray_linalg;
+extern crate num_traits;
 
 use ndarray::*;
 use ndarray_linalg::*;
-use itertools::iterate;
-use std::mem::replace;
-
-use super::traits::*;
-
-pub use ndarray::linalg::Dot;
-
-/// Jacobian operator using numerical-differentiation
-pub struct Jacobian<'a, TEO>
-    where TEO: 'a,
-          &'a TEO: TimeEvolution<f64, OwnedRcRepr<f64>, Ix1>
-{
-    f: &'a TEO,
-    x: RcArray1<f64>,
-    fx: RcArray1<f64>,
-    alpha: f64,
-}
-
-pub fn jacobian<'a, TEO>(f: &'a TEO, x: RcArray1<f64>, alpha: f64) -> Jacobian<'a, TEO>
-    where TEO: 'a,
-          &'a TEO: TimeEvolution<f64, OwnedRcRepr<f64>, Ix1>
-{
-    let fx = x.clone();
-    f.iterate(&mut fx);
-    Jacobian {
-        f: f,
-        x: x,
-        fx: fx,
-        alpha: alpha,
-    }
-}
-
-impl<'a, S, TEO> Dot<ArrayBase<S, Ix1>> for Jacobian<'a, TEO>
-    where &'a TEO: TimeEvolution<f64, OwnedRcRepr<f64>, Ix1>,
-          S: Data<Elem = f64>
-{
-    type Output = RcArray1<f64>;
-    fn dot(&self, dx: &ArrayBase<S, Ix1>) -> Self::Output {
-        let nrm = self.x.norm_l2().max(dx.norm_l2());
-        let n = self.alpha / nrm;
-        let x = n * dx + &self.x;
-        (*self.f.iterate(&mut x.into_shared()) - &self.fx) / n
-    }
-}
-
-impl<'a, S, TEO> Dot<ArrayBase<S, Ix2>> for Jacobian<'a, TEO>
-    where &'a TEO: TimeEvolution<f64, OwnedRcRepr<f64>, Ix1>,
-          S: Data<Elem = f64>
-{
-    type Output = Array2<f64>;
-    fn dot(&self, dxs: &ArrayBase<S, Ix2>) -> Self::Output {
-        hstack(&dxs.axis_iter(Axis(1))
-                   .map(|dx| self.dot(&dx))
-                   .collect::<Vec<_>>())
-            .unwrap()
-    }
-}
-
-fn clv_backward(c: &Array2<f64>, r: &Array2<f64>) -> (Array2<f64>, Array1<f64>) {
-    let cd = r.solve_triangular(UPLO::Upper, ::ndarray_linalg::Diag::NonUnit, c)
-        .expect("Failed to solve R");
-    let (c, d) = normalize(cd, NormalizeAxis::Column);
-    let f = Array::from_vec(d).mapv_into(|x| 1.0 / x);
-    (c, f)
-}
+use ndarray_odeint::*;
+use num_traits::Float;
 
 /// Calculate all Lyapunov exponents
-pub fn exponents<'a, TEO>(teo: &'a TEO,
-                          x0: RcArray1<f64>,
-                          alpha: f64,
-                          duration: usize)
-                          -> Array1<f64>
-    where &'a TEO: TimeEvolution<f64, OwnedRcRepr<f64>, Ix1>,
-          TEO: 'a + TimeStep<f64>
+pub fn exponents<A, TEO>(teo: TEO, x0: Array1<A>, alpha: A::Real, duration: usize) -> Array1<A>
+    where A: RealScalar,
+          TEO: TimeStep<A::Real>,
+          for<'a> &'a TEO: TimeEvolution<OwnedRepr<A>, Ix1>,
+          for<'a, 'b> Jacobian<'a, A, OwnedRepr<A>, Ix1, TEO>: OperatorMut<ViewRepr<&'b mut A>, Ix1>
 {
     let n = x0.len();
-    let ts = iterate(x0, |y| teo.iterate(&mut y));
-    ts.scan(Array::eye(n), |q, x| {
-        let (q_next, r): (_, Array2<f64>) = jacobian(teo, x.clone(), alpha).dot(q).qr().unwrap();
+    let dur: A = AssociatedReal::inject(teo.get_dt() * into_scalar(duration as f64));
+    let ts = TimeSeries::new(x0, &teo);
+    ts.scan(Array::eye(n), |q: &mut Array2<A>, x| {
+        let j = jacobian(&teo, x.clone(), alpha);
+        let q = j.op_multi_mut(q);
+        let (q_next, r) = q.qr().unwrap();
         *q = q_next;
-        let d = r.diag().map(|x: &f64| x.abs().ln());
+        let d: Array1<A> = r.diag().map(|x| AssociatedReal::inject(x.abs().ln()));
         Some(d)
     }).skip(duration / 10)
         .take(duration)
-        .fold(Array::zeros(n), |x, y| x + y) / (teo.get_dt() * duration as f64)
+        .fold(ArrayBase::zeros(n), |mut x, y| {
+            azip!(mut x, y in { *x = *x + y/dur } );
+            x
+        })
 }
 
-/// Calculate Covariant Lyapunov Vector
-///
-/// **CAUTION**
-/// This function consumes much memory since this saves matrices duraing the time evolution.
-pub fn clv<'a, TEO>(teo: &'a TEO,
-                    x0: RcArray1<f64>,
-                    alpha: f64,
-                    duration: usize)
-                    -> Vec<(Array1<f64>, Array2<f64>, Array1<f64>)>
-    where &'a TEO: TimeEvolution<f64, OwnedRcRepr<f64>, Ix1>,
-          TEO: 'a
-{
-    let n = x0.len();
-    let ts = iterate(x0, |y| teo.iterate(y.clone()));
-    let qr_series = ts.scan(Array::eye(n), |q, x| {
-        let (q_next, r) = jacobian(teo, x.clone(), alpha).dot(q).qr().unwrap();
-        let q = replace(q, q_next);
-        Some((x, q, r))
-    }).skip(duration / 10)
-        .take(duration + duration / 10)
-        .collect::<Vec<_>>();
-    let clv_rev = qr_series
-        .into_iter()
-        .rev()
-        .scan(Array::eye(n), |c, (x, q, r)| {
-            let (c_now, f) = clv_backward(c, &r);
-            let v = q.dot(&c_now);
-            *c = c_now;
-            Some((x.into_owned(), v, f))
-        })
-        .collect::<Vec<_>>();
-    clv_rev.into_iter().skip(duration / 10).rev().collect()
+fn main() {
+    let dt = 0.01;
+    let eom = model::Lorenz63::default();
+    let teo = explicit::rk4(eom, dt);
+    let l = exponents(teo, arr1(&[1.0, 0.0, 0.0]), 1e-7, 100000);
+    println!("Lyapunov Exp = {:?}", l);
+    assert_close_l2!(&l, &arr1(&[0.906, 0.0, -14.572]), 1e-2);
+    // value from http://sprott.physics.wisc.edu/chaos/lorenzle.htm
 }
