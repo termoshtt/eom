f6a14fce79d08066eee4589d48d8150f422f25fb
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-07-13 18:21:21 +0900

    NStep adaptor

--- src/adaptor.rs
+++ src/adaptor.rs
@@ -1,5 +1,6 @@
 
 use ndarray::*;
+use ndarray_linalg::*;
 use super::traits::*;
 
 pub struct TimeSeries<'a, TEO, S, D>
@@ -41,5 +42,50 @@ impl<'a, TEO, S, D> Iterator for TimeSeries<'a, TEO, S, D>
     fn next(&mut self) -> Option<Self::Item> {
         self.iterate();
         Some(self.state.clone())
+    }
+}
+
+#[derive(new)]
+pub struct NStep<TEO> {
+    teo: TEO,
+    n: usize,
+}
+
+impl<TEO, D> ModelSize<D> for NStep<TEO>
+    where TEO: ModelSize<D>,
+          D: Dimension
+{
+    fn model_size(&self) -> D::Pattern {
+        self.teo.model_size()
+    }
+}
+
+impl<TEO> TimeStep for NStep<TEO>
+    where TEO: TimeStep
+{
+    type Time = TEO::Time;
+
+    fn get_dt(&self) -> Self::Time {
+        self.teo.get_dt() * into_scalar(self.n as f64)
+    }
+
+    fn set_dt(&mut self, dt: Self::Time) {
+        self.teo.set_dt(dt / into_scalar(self.n as f64));
+    }
+}
+
+impl<TEO, S, D> TimeEvolutionBase<S, D> for NStep<TEO>
+    where TEO: TimeEvolutionBase<S, D>,
+          S: DataMut,
+          D: Dimension
+{
+    type Scalar = TEO::Scalar;
+    type Time = TEO::Time;
+
+    fn iterate<'a>(&self, x: &'a mut ArrayBase<S, D>) -> &'a mut ArrayBase<S, D> {
+        for _ in 0..self.n {
+            self.teo.iterate(x);
+        }
+        x
     }
 }
