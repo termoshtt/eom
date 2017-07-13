9f043128bbdad4852ae85af506de195007ffa002
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-07-13 20:06:51 +0900

    Rewrite stiff-rk4 algorithm using SemiImplicit{NonLinear,Linear}

--- src/semi_implicit.rs
+++ src/semi_implicit.rs
@@ -1,67 +1,36 @@
 //! Define semi-implicit schemes
 
 use super::traits::*;
-use super::diag::Diagonal;
 
 use ndarray::*;
-use ndarray_linalg::{Scalar, into_scalar};
+use ndarray_linalg::*;
 
-pub struct DiagRK4<A, S, F, D>
-    where A: Scalar,
-          S: Data<Elem = A>,
-          D: Dimension
-{
-    f: F,
-    lin_half: Diagonal<A, S, D>,
-    dt: A::Real,
+pub struct StiffRK4<NLin, Lin, Time: RealScalar> {
+    nlin: NLin,
+    lin: Lin,
+    dt: Time,
 }
 
-pub fn diag_rk4<A, S, F, D>(f: F, dt: A::Real) -> DiagRK4<A, S, F, D>
-    where A: Scalar,
-          S: DataClone<Elem = A> + DataMut,
-          F: SemiImplicitDiag<S, S, D>,
-          D: Dimension
-{
-    DiagRK4::new(f, dt)
-}
-
-impl<A, S, F, D> DiagRK4<A, S, F, D>
-    where A: Scalar,
-          S: DataClone<Elem = A> + DataMut,
-          D: Dimension
-{
-    pub fn new(f: F, dt: A::Real) -> Self
-        where F: SemiImplicitDiag<S, S, D>
-    {
-        let diag = f.diag();
-        let lin_half = Diagonal::new(diag, dt / into_scalar(2.0));
-        DiagRK4 {
-            f: f,
-            lin_half: lin_half,
-            dt: dt,
-        }
-    }
-}
-
-impl<A, S, F, D> ModelSize<D> for DiagRK4<A, S, F, D>
-    where A: Scalar,
-          F: ModelSize<D>,
-          S: DataClone<Elem = A> + DataMut,
-          D: Dimension
+impl<D, NLin, Lin, Time> ModelSize<D> for StiffRK4<NLin, Lin, Time>
+    where D: Dimension,
+          NLin: ModelSize<D>,
+          Lin: ModelSize<D>,
+          Time: RealScalar
 {
     fn model_size(&self) -> D::Pattern {
-        self.f.model_size()
+        self.nlin.model_size() // TODO check
     }
 }
 
-impl<A, S, F, D> TimeEvolutionBase<S, D> for DiagRK4<A, S, F, D>
+impl<A, S, D, NLin, Lin> TimeEvolutionBase<S, D> for StiffRK4<NLin, Lin, A::Real>
     where A: Scalar,
-          S: DataMut<Elem = A> + DataClone + DataOwned,
-          F: SemiImplicitDiag<S, S, D, Time = A::Real, Scalar = A>,
-          D: Dimension
+          S: DataMut<Elem = A> + DataOwned,
+          D: Dimension,
+          NLin: SemiImplicitNonLinear<S, D, Scalar = A, Time = A::Real>,
+          Lin: SemiImplicitLinear<S, D, Scalar = A, Time = A::Real>
 {
-    type Scalar = F::Scalar;
-    type Time = F::Time;
+    type Scalar = A;
+    type Time = A::Real;
 
     fn iterate<'a>(&self, x: &'a mut ArrayBase<S, D>) -> &'a mut ArrayBase<S, D> {
         // constants
@@ -70,18 +39,18 @@ impl<A, S, F, D> TimeEvolutionBase<S, D> for DiagRK4<A, S, F, D>
         let dt_3 = self.dt / into_scalar(3.0);
         let dt_6 = self.dt / into_scalar(6.0);
         // operators
-        let l = &self.lin_half;
-        let f = &self.f;
+        let l = &self.lin;
+        let f = &self.nlin;
         // calc
-        let mut x_ = x.to_owned();
-        let mut lx = x.to_owned();
-        l.iterate(&mut lx);
+        let mut x_ = replicate(&x);
+        let mut lx = replicate(&x);
+        l.lin(&mut lx);
         let mut k1 = f.nlin(x);
         let k1_ = k1.to_owned();
         Zip::from(&mut *k1)
             .and(&x_)
             .apply(|k1, &x_| { *k1 = x_ + k1.mul_real(dt_2); });
-        let mut k2 = f.nlin(l.iterate(k1));
+        let mut k2 = f.nlin(l.lin(k1));
         let k2_ = k2.to_owned();
         Zip::from(&mut *k2)
             .and(&lx)
@@ -91,11 +60,11 @@ impl<A, S, F, D> TimeEvolutionBase<S, D> for DiagRK4<A, S, F, D>
         Zip::from(&mut *k3)
             .and(&lx)
             .apply(|k3, &lx| { *k3 = lx + k3.mul_real(dt); });
-        let mut k4 = f.nlin(l.iterate(k3));
+        let mut k4 = f.nlin(l.lin(k3));
         azip!(mut x_, k1_ in { *x_ = *x_ + k1_.mul_real(dt_6) });
-        l.iterate(&mut x_);
+        l.lin(&mut x_);
         azip!(mut x_, k2_, k3_ in { *x_ = *x_ + (k2_ + k3_).mul_real(dt_3) });
-        l.iterate(&mut x_);
+        l.lin(&mut x_);
         Zip::from(&mut *k4)
             .and(&x_)
             .apply(|k4, &x_| { *k4 = x_ + k4.mul_real(dt_6); });
