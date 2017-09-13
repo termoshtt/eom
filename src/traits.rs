3d69a9c0d1a0a45bdbd07b84102138fd3cc58d8c
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-09-13 14:56:05 +0900

    Simplify traits

--- src/traits.rs
+++ src/traits.rs
@@ -15,60 +15,24 @@ pub trait TimeStep {
 }
 
 /// Equation of motion (Explicit)
-pub trait Explicit<S, D>: ModelSize<D>
-    where S: DataMut,
-          D: Dimension
-{
+pub trait Explicit<D: Dimension>: ModelSize<D> {
     type Scalar: Scalar;
     type Time: RealScalar;
     /// calculate right hand side (rhs) of Explicit from current state
-    fn rhs<'a>(&self, &'a mut ArrayBase<S, D>) -> &'a mut ArrayBase<S, D>;
+    fn rhs<S>(&self, &mut ArrayBase<S, D>) where S: DataMut<Elem = Self::Scalar>;
 }
 
-pub trait SemiImplicit<S, D>: ModelSize<D>
-    where S: DataMut,
-          D: Dimension
-{
+pub trait SemiImplicit<D: Dimension>: ModelSize<D> {
     type Scalar: Scalar;
     type Time: RealScalar;
     /// non-linear part of stiff equation
-    fn nlin<'a>(&self, &'a mut ArrayBase<S, D>) -> &'a mut ArrayBase<S, D>;
-}
-
-/// Time-evolution operator
-pub trait TimeEvolutionBase<S, D>: ModelSize<D> + TimeStep
-    where S: DataMut,
-          D: Dimension
-{
-    type Scalar: Scalar;
-    /// calculate next step
-    fn iterate<'a>(&self, &'a mut ArrayBase<S, D>) -> &'a mut ArrayBase<S, D>;
-}
-
-pub trait TimeEvolution<A, D>
-    : TimeEvolutionBase<OwnedRepr<A>, D, Scalar = A, Time = A::Real>
-    + TimeEvolutionBase<OwnedRcRepr<A>, D, Scalar = A, Time = A::Real>
-    + for<'a> TimeEvolutionBase<ViewRepr<&'a mut A>, D, Scalar = A, Time = A::Real>
-    where A: Scalar,
-          D: Dimension
-{
+    fn nlin<S>(&self, &mut ArrayBase<S, D>) where S: DataMut<Elem = Self::Scalar>;
 }
 
 /// Time-evolution operator with buffer
-pub trait TimeEvolutionBufferedBase<S, D, Buffer>: ModelSize<D> + TimeStep
-    where S: DataMut,
-          D: Dimension
-{
+pub trait TimeEvolution<D: Dimension>: ModelSize<D> + TimeStep {
     type Scalar: Scalar;
+    type Buffer;
     /// calculate next step
-    fn iterate_buf<'a>(&self, &'a mut ArrayBase<S, D>, &mut Buffer) -> &'a mut ArrayBase<S, D>;
-}
-
-pub trait TimeEvolutionBuffered<A, D, Buffer>
-    : TimeEvolutionBufferedBase<OwnedRepr<A>, D, Buffer, Scalar = A, Time = A::Real>
-    + TimeEvolutionBufferedBase<OwnedRcRepr<A>, D, Buffer, Scalar = A, Time = A::Real>
-    + for<'a> TimeEvolutionBufferedBase<ViewRepr<&'a mut A>, D, Buffer, Scalar = A, Time = A::Real>
-    where A: Scalar,
-          D: Dimension
-{
+    fn iterate<S>(&self, &mut ArrayBase<S, D>, &mut Buffer) where S: DataMut<Elem = Self::Scalar>;
 }
