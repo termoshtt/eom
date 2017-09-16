4c4611df8c5feb2293886cd2bb0d8804f102ed3d
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-09-16 21:21:10 +0900

    implmenet explicit schemes for ExplicitBuf

--- src/explicit.rs
+++ src/explicit.rs
@@ -7,12 +7,12 @@ use super::traits::*;
 macro_rules! def_explicit {
     ($method:ident, $constructor:ident) => {
 
-pub struct $method<F: Explicit> {
+pub struct $method<F: ExplicitBuf> {
     f: F,
     dt: <F::Scalar as AssociatedReal>::Real,
 }
 
-impl<D: Dimension, F: Explicit<Dim = D>> ModelSpec for $method<F> {
+impl<D: Dimension, F: ExplicitBuf<Dim = D>> ModelSpec for $method<F> {
     type Dim = D;
 
     fn model_size(&self) -> D::Pattern {
@@ -20,7 +20,7 @@ impl<D: Dimension, F: Explicit<Dim = D>> ModelSpec for $method<F> {
     }
 }
 
-impl<A: Scalar, F: Explicit<Scalar = A>> TimeStep for $method<F> {
+impl<A: Scalar, F: ExplicitBuf<Scalar = A>> TimeStep for $method<F> {
     type Time = A::Real;
     fn get_dt(&self) -> Self::Time {
         self.dt
@@ -32,7 +32,7 @@ impl<A: Scalar, F: Explicit<Scalar = A>> TimeStep for $method<F> {
 
 pub fn $constructor<A, F>(f: F, dt: A::Real) -> $method<F>
 where A: Scalar,
-      F: Explicit<Scalar = A>
+      F: ExplicitBuf<Scalar = A>
 {
     $method { f: f, dt: dt }
 }
@@ -43,22 +43,26 @@ def_explicit!(Euler, euler);
 def_explicit!(Heun, heun);
 def_explicit!(RK4, rk4);
 
-pub struct EulerBuffer<A, D> {
-    x: Array<A, D>,
+pub struct EulerBuffer<ExBuf, Arr> {
+    ex: ExBuf,
+    x: Arr,
 }
 
 impl<F> BufferSpec for Euler<F>
-    where F: Explicit + ModelSpec
+    where F: ExplicitBuf + ModelSpec
 {
-    type Buffer = EulerBuffer<F::Scalar, F::Dim>;
+    type Buffer = EulerBuffer<F::Buffer, Array<F::Scalar, F::Dim>>;
 
     fn new_buffer(&self) -> Self::Buffer {
-        EulerBuffer { x: Array::zeros(self.f.model_size()) }
+        EulerBuffer {
+            ex: self.f.new_buffer(),
+            x: Array::zeros(self.f.model_size()),
+        }
     }
 }
 
 impl<F> TimeEvolution for Euler<F>
-    where F: Explicit + ModelSpec
+    where F: ExplicitBuf + ModelSpec
 {
     type Scalar = F::Scalar;
 
@@ -69,7 +73,7 @@ impl<F> TimeEvolution for Euler<F>
         where S: DataMut<Elem = Self::Scalar>
     {
         buf.x.zip_mut_with(x, |buf, x| *buf = *x);
-        let mut fx = self.f.rhs(x);
+        let mut fx = self.f.rhs(x, &mut buf.ex);
         Zip::from(&mut *fx)
             .and(&buf.x)
             .apply(|vfx, vx| { *vfx = *vx + vfx.mul_real(self.dt); });
@@ -77,23 +81,25 @@ impl<F> TimeEvolution for Euler<F>
     }
 }
 
-pub struct HeunBuffer<A, D> {
-    x: Array<A, D>,
-    k1: Array<A, D>,
+pub struct HeunBuffer<ExBuf, Arr> {
+    ex: ExBuf,
+    x: Arr,
+    k1: Arr,
 }
 
-impl<F: Explicit + ModelSpec> BufferSpec for Heun<F> {
-    type Buffer = HeunBuffer<F::Scalar, F::Dim>;
+impl<F: ExplicitBuf + ModelSpec> BufferSpec for Heun<F> {
+    type Buffer = HeunBuffer<F::Buffer, Array<F::Scalar, F::Dim>>;
 
     fn new_buffer(&self) -> Self::Buffer {
         HeunBuffer {
+            ex: self.f.new_buffer(),
             x: Array::zeros(self.f.model_size()),
             k1: Array::zeros(self.f.model_size()),
         }
     }
 }
 
-impl<F: Explicit + ModelSpec> TimeEvolution for Heun<F> {
+impl<F: ExplicitBuf + ModelSpec> TimeEvolution for Heun<F> {
     type Scalar = F::Scalar;
 
     fn iterate<'a, S>(&self,
@@ -106,12 +112,12 @@ impl<F: Explicit + ModelSpec> TimeEvolution for Heun<F> {
         let dt_2 = self.dt * into_scalar(0.5);
         // calc
         buf.x.zip_mut_with(x, |buf, x| *buf = *x);
-        let k1 = self.f.rhs(x);
+        let k1 = self.f.rhs(x, &mut buf.ex);
         buf.k1.zip_mut_with(k1, |buf, k1| *buf = *k1);
         Zip::from(&mut *k1)
             .and(&buf.x)
             .apply(|k1, &x_| { *k1 = k1.mul_real(dt) + x_; });
-        let k2 = self.f.rhs(k1);
+        let k2 = self.f.rhs(k1, &mut buf.ex);
         Zip::from(&mut *k2)
             .and(&buf.x)
             .and(&buf.k1)
@@ -120,18 +126,20 @@ impl<F: Explicit + ModelSpec> TimeEvolution for Heun<F> {
     }
 }
 
-pub struct RK4Buffer<A, D> {
-    x: Array<A, D>,
-    k1: Array<A, D>,
-    k2: Array<A, D>,
-    k3: Array<A, D>,
+pub struct RK4Buffer<ExBuf, Arr> {
+    ex: ExBuf,
+    x: Arr,
+    k1: Arr,
+    k2: Arr,
+    k3: Arr,
 }
 
-impl<F: Explicit + ModelSpec> BufferSpec for RK4<F> {
-    type Buffer = RK4Buffer<F::Scalar, F::Dim>;
+impl<F: ExplicitBuf + ModelSpec> BufferSpec for RK4<F> {
+    type Buffer = RK4Buffer<F::Buffer, Array<F::Scalar, F::Dim>>;
 
     fn new_buffer(&self) -> Self::Buffer {
         RK4Buffer {
+            ex: self.f.new_buffer(),
             x: Array::zeros(self.f.model_size()),
             k1: Array::zeros(self.f.model_size()),
             k2: Array::zeros(self.f.model_size()),
@@ -140,7 +148,7 @@ impl<F: Explicit + ModelSpec> BufferSpec for RK4<F> {
     }
 }
 
-impl<F: Explicit + ModelSpec> TimeEvolution for RK4<F> {
+impl<F: ExplicitBuf + ModelSpec> TimeEvolution for RK4<F> {
     type Scalar = F::Scalar;
 
     fn iterate<'a, S>(&self,
@@ -155,24 +163,24 @@ impl<F: Explicit + ModelSpec> TimeEvolution for RK4<F> {
         let dt_6 = self.dt / into_scalar(6.0);
         buf.x.zip_mut_with(x, |buf, x| *buf = *x);
         // k1
-        let mut k1 = self.f.rhs(x);
+        let mut k1 = self.f.rhs(x, &mut buf.ex);
         buf.k1.zip_mut_with(k1, |buf, k1| *buf = *k1);
         Zip::from(&mut *k1)
             .and(&buf.x)
             .apply(|k1, &x| { *k1 = k1.mul_real(dt_2) + x; });
         // k2
-        let mut k2 = self.f.rhs(k1);
+        let mut k2 = self.f.rhs(k1, &mut buf.ex);
         buf.k2.zip_mut_with(k2, |buf, k| *buf = *k);
         Zip::from(&mut *k2)
             .and(&buf.x)
             .apply(|k2, &x| { *k2 = x + k2.mul_real(dt_2); });
         // k3
-        let mut k3 = self.f.rhs(k2);
+        let mut k3 = self.f.rhs(k2, &mut buf.ex);
         buf.k3.zip_mut_with(k3, |buf, k| *buf = *k);
         Zip::from(&mut *k3)
             .and(&buf.x)
             .apply(|k3, &x| { *k3 = x + k3.mul_real(dt); });
-        let mut k4 = self.f.rhs(k3);
+        let mut k4 = self.f.rhs(k3, &mut buf.ex);
         Zip::from(&mut *k4)
             .and(&buf.x)
             .and(&buf.k1)
