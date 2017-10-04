52e8ce2b836e2a330312a5d3d0a9fbf3f8cdde8f
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-10-04 15:06:43 +0900

    Add example of KSE

---
+++ examples/kse.rs
@@ -0,0 +1,43 @@
+
+extern crate ndarray;
+extern crate eom;
+extern crate num_traits;
+extern crate num_complex;
+
+use ndarray::*;
+use eom::*;
+use num_complex::Complex64 as c64;
+use num_traits::Zero;
+
+fn main() {
+    let n = 128;
+    let l = 100.0;
+    let dt = 1e-4;
+    let eom = pde::KSE::new(n, l);
+    let n_coef = eom.model_size();
+    let teo = semi_implicit::diag_rk4(eom, dt);
+    let mut x0 = arr1(&vec![c64::zero(); n_coef]);
+    x0[2] = c64::new(1.0, 0.0);
+    x0[3] = c64::new(1.0, 0.0);
+    x0[4] = c64::new(1.0, 0.0);
+    x0[5] = c64::new(1.0, 0.0);
+    x0[6] = c64::new(1.0, 0.0);
+    let ts = time_series(x0, &teo);
+    let end_time = 1_000_000;
+    let interval = 100;
+    print!("time");
+    for i in 0..n_coef {
+        print!(",r{},c{}", i, i);
+    }
+    println!("");
+    for (t, v) in ts.take(end_time).enumerate() {
+        if t % interval != 0 {
+            continue;
+        }
+        print!("{:e}", dt * t as f64);
+        for c in v.iter() {
+            print!(",{:e},{:e}", c.re, c.im);
+        }
+        println!("");
+    }
+}
--- src/pde/mod.rs
+++ src/pde/mod.rs
@@ -1,2 +1,3 @@
 
 pub mod kse;
+pub use self::kse::KSE;
