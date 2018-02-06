6ab528bc8f623981e863d0bcd2a0214ea6df4625
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-02-06 23:07:32 +0900

    Simplify examples/accuracy

--- examples/accuracy.rs
+++ examples/accuracy.rs
@@ -12,18 +12,20 @@ use num_traits::int::PrimInt;
 use eom::*;
 use eom::traits::*;
 
-fn check_accuracy<Sc>(fname: &str)
+fn check_accuracy<A, D, F, Sc>(mut teo: Sc, init: Array<A, D>, fname: &str)
 where
-    Sc: Scheme<ode::Lorenz63, Scalar = f64, Dim = Ix1, Time = f64>,
+    A: Scalar<Real = f64>,
+    D: Dimension,
+    F: Explicit<Scalar = A, Dim = D>,
+    Sc: Scheme<F, Scalar = A, Dim = D, Time = f64>,
 {
     let data: Vec<_> = (0..12)
         .map(|n| {
-            let dt = 0.1 / 2.pow(n) as f64;
-            let eom = ode::Lorenz63::default();
-            let mut teo = Sc::new(eom, dt);
-            let t = 100 * 2.pow(n);
-            let ts = adaptor::time_series(arr1(&[1.0, 0.0, 0.0]), &mut teo);
-            (dt, ts.take(t).last().unwrap())
+            let dt = 0.01 / 2.pow(n) as f64;
+            let t = 1000 * 2.pow(n);
+            teo.set_dt(dt);
+            let mut ts = adaptor::time_series(init.clone(), &mut teo);
+            (dt, ts.nth(t - 1).unwrap())
         })
         .collect();
 
@@ -32,12 +34,25 @@ where
     for i in 0..(data.len() - 1) {
         let dt = data[i].0;
         let dev = (&data[i + 1].1 - &data[i].1).norm();
-        write!(&mut f, "{},{}\n", dt, dev).unwrap();
+        write!(&mut f, "{:.08e},{:.08e}\n", dt, dev).unwrap();
     }
 }
 
 fn main() {
-    check_accuracy::<explicit::Euler<ode::Lorenz63>>("euler.csv");
-    check_accuracy::<explicit::Heun<ode::Lorenz63>>("heun.csv");
-    check_accuracy::<explicit::RK4<ode::Lorenz63>>("rk4.csv");
+    let l63 = ode::Lorenz63::default();
+    check_accuracy(
+        explicit::Euler::new(l63.clone(), 1.0),
+        arr1(&[1.0, 0.0, 0.0]),
+        "euler.csv",
+    );
+    check_accuracy(
+        explicit::Heun::new(l63.clone(), 1.0),
+        arr1(&[1.0, 0.0, 0.0]),
+        "heun.csv",
+    );
+    check_accuracy(
+        explicit::RK4::new(l63.clone(), 1.0),
+        arr1(&[1.0, 0.0, 0.0]),
+        "rk4.csv",
+    );
 }
