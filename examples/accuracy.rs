6bd9227c3b6361b2e867f76ac4553f17b95c974d
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-02-07 07:31:33 +0900

    Restore accuracy check of diag_rk4

--- examples/accuracy.rs
+++ examples/accuracy.rs
@@ -12,28 +12,40 @@ use num_traits::int::PrimInt;
 use eom::*;
 use eom::traits::*;
 
-fn check_accuracy<A, D, F, Sc>(mut teo: Sc, init: Array<A, D>, fname: &str)
+fn accuracy<A, D, F, Sc>(mut teo: Sc, init: Array<A, D>) -> Vec<(Sc::Time, A::Real)>
 where
-    A: Scalar<Real = f64>,
+    A: Scalar,
     D: Dimension,
-    F: Explicit<Scalar = A, Dim = D>,
-    Sc: Scheme<F, Scalar = A, Dim = D, Time = f64>,
+    Sc: Scheme<F, Scalar = A, Dim = D>,
 {
     let data: Vec<_> = (0..12)
         .map(|n| {
-            let dt = 0.01 / 2.pow(n) as f64;
+            let dt = into_scalar(0.01 / 2.pow(n) as f64);
             let t = 1000 * 2.pow(n);
             teo.set_dt(dt);
             let mut ts = adaptor::time_series(init.clone(), &mut teo);
             (dt, ts.nth(t - 1).unwrap())
         })
         .collect();
+    data.windows(2)
+        .map(|w| {
+            let dt = w[0].0;
+            let dev = (&w[1].1 - &w[0].1).norm();
+            (dt, dev)
+        })
+        .collect()
+}
 
+fn check_accuracy<A, D, F, Sc>(teo: Sc, init: Array<A, D>, fname: &str)
+where
+    A: Scalar<Real = f64>,
+    D: Dimension,
+    Sc: Scheme<F, Scalar = A, Dim = D, Time = f64>,
+{
+    let acc = accuracy(teo, init);
     let mut f = File::create(fname).unwrap();
     write!(&mut f, "dt,dev\n").unwrap();
-    for i in 0..(data.len() - 1) {
-        let dt = data[i].0;
-        let dev = (&data[i + 1].1 - &data[i].1).norm();
+    for &(dt, dev) in acc.iter() {
         write!(&mut f, "{:.08e},{:.08e}\n", dt, dev).unwrap();
     }
 }
@@ -54,5 +66,10 @@ fn main() {
         explicit::RK4::new(l63.clone(), 1.0),
         arr1(&[1.0, 0.0, 0.0]),
         "rk4.csv",
+    );
+    check_accuracy(
+        semi_implicit::DiagRK4::new(l63.clone(), 1.0),
+        arr1(&[1.0, 0.0, 0.0]),
+        "diag_rk4.csv",
     );
 }
