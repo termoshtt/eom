d2f463b265f46bc826cd7cf4a873e897792c3ab7
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-02-08 23:48:48 +0900

    Add Pair

--- src/pde/kse.rs
+++ src/pde/kse.rs
@@ -6,6 +6,39 @@ use std::f64::consts::PI;
 
 use traits::*;
 
+/// Pair of one-dimensional Real/Complex aligned arrays
+pub struct Pair {
+    pub r: AlignedVec<f64>,
+    pub c: AlignedVec<c64>,
+    r2c: R2CPlan64,
+    c2r: C2RPlan64,
+}
+
+impl Pair {
+    pub fn new(n: usize) -> Self {
+        let nf = n / 2 + 1;
+        let mut r = AlignedVec::new(n);
+        let mut c = AlignedVec::new(nf);
+        let r2c = R2CPlan::new(&[n], &mut r, &mut c, Flag::Measure).unwrap();
+        let c2r = C2RPlan::new(&[n], &mut c, &mut r, Flag::Measure).unwrap();
+        Pair { r, c, r2c, c2r }
+    }
+
+    pub fn r2c(&mut self) {
+        self.r2c.r2c(&mut self.r, &mut self.c).unwrap();
+    }
+
+    pub fn c2r(&mut self) {
+        self.c2r.c2r(&mut self.c, &mut self.r).unwrap();
+    }
+}
+
+impl Clone for Pair {
+    fn clone(&self) -> Self {
+        Pair::new(self.r.len())
+    }
+}
+
 pub struct KSE {
     n: usize,
     nf: usize,
