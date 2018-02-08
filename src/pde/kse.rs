cf0219b2db7ed779b144f245978dbe65ceb05e5c
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-02-09 01:37:32 +0900

    Use Pair in KSE

--- src/pde/kse.rs
+++ src/pde/kse.rs
@@ -44,12 +44,8 @@ pub struct KSE {
     nf: usize,
     length: f64,
     k: Array1<c64>,
-    u: AlignedVec<f64>,
-    ux: AlignedVec<f64>,
-    uf: AlignedVec<c64>,
-    uxf: AlignedVec<c64>,
-    r2c: R2CPlan64,
-    c2r: C2RPlan64,
+    u: Pair,
+    ux: Pair,
 }
 
 impl Clone for KSE {
@@ -70,34 +66,14 @@ impl KSE {
     pub fn new(n: usize, length: f64) -> Self {
         let nf = n / 2 + 1;
         let k0 = 2.0 * PI / length;
-        let k = Array::from_iter((0..nf).map(|i| c64::new(0.0, k0 * i as f64)));
-        let mut u = AlignedVec::new(n);
-        let mut uf = AlignedVec::new(nf);
-        let ux = AlignedVec::new(n);
-        let uxf = AlignedVec::new(nf);
-        let r2c = R2CPlan::new(&[n], &mut u, &mut uf, Flag::Measure).unwrap();
-        let c2r = C2RPlan::new(&[n], &mut uf, &mut u, Flag::Measure).unwrap();
         KSE {
             n,
             nf,
             length,
-            k,
-            u,
-            ux,
-            uf,
-            uxf,
-            r2c,
-            c2r,
+            k: Array::from_iter((0..nf).map(|i| c64::new(0.0, k0 * i as f64))),
+            u: Pair::new(n),
+            ux: Pair::new(n),
         }
-    }
-
-    /// Convert the coefficient to the field value for easy visualization
-    pub fn convert_u<'a>(&'a mut self, uf: &[c64]) -> &'a [f64] {
-        for (c, uc) in self.uf.iter_mut().zip(uf.iter()) {
-            *c = *uc;
-        }
-        self.c2r.c2r(&mut self.uf, &mut self.u).unwrap();
-        &self.u
     }
 }
 
@@ -110,17 +86,17 @@ impl SemiImplicit for KSE {
         S: DataMut<Elem = Self::Scalar>,
     {
         for i in 0..self.nf {
-            self.uf[i] = uf[i];
-            self.uxf[i] = self.k[i] * uf[i];
+            self.u.c[i] = uf[i];
+            self.ux.c[i] = self.k[i] * uf[i];
         }
-        self.c2r.c2r(&mut self.uf, &mut self.u).unwrap();
-        self.c2r.c2r(&mut self.uxf, &mut self.ux).unwrap();
-        for (u, ux) in self.u.iter_mut().zip(self.ux.iter()) {
+        self.u.c2r();
+        self.ux.c2r();
+        for (u, ux) in self.u.r.iter_mut().zip(self.ux.r.iter()) {
             *u = -*u * *ux;
         }
-        self.r2c.r2c(&mut self.u, &mut self.uf).unwrap();
+        self.u.r2c();
         let normalize = 1.0 / self.n as f64;
-        for (u_, u) in uf.iter_mut().zip(self.uf.iter()) {
+        for (u_, u) in uf.iter_mut().zip(self.u.c.iter()) {
             *u_ = *u * normalize;
         }
         uf
