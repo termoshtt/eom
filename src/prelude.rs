0127003ced0ad29e26c8673a769e409b2cd4db6b
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-02-14 06:08:09 +0900

    Drop unused prelude mod

--- src/prelude.rs
+++
@@ -1,6 +0,0 @@
-pub use super::traits::*;
-pub use super::{explicit, semi_implicit};
-pub use super::lorenz63::Lorenz63;
-pub use super::lorenz96::Lorenz96;
-pub use super::roessler::Roessler;
-pub use super::goy_shell::GoyShell;
