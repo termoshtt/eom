07fdbe4a29cd45f8e9e47da79f421c3e18fddf90
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-02-14 06:08:34 +0900

    Merge jacobian module into lyapunov module

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,4 @@
 //! solve ODE with rust-ndarray
-//!
-//! Algorithms
-//! -----------
-//!
-//! - explicit
-//!   - Euler
-//!   - Heun
-//!   - classical 4th order Runge-Kutta
-//! - semi-implicit
-//!   - stiff RK4
-//!
-//! Models
-//! -------
-//! Basic chaotic dynamics are implemented as examples in [model submodule](model/)
-//!
-//! - [Lorenz three-variables system](https://en.wikipedia.org/wiki/Lorenz_system): [Lorenz63](model/lorenz63/struct.Lorenz63.html)
-//! - [Lorenz 96 system](https://en.wikipedia.org/wiki/Lorenz_96_model): [Lorenz96](model/lorenz96/struct.Lorenz96.html)
-//! - [Roessler system](https://en.wikipedia.org/wiki/R%C3%B6ssler_attractor): [Roessler](model/roessler/struct.Roessler.html)
-//! - GOY shell model: [GoyShell](model/goy_shell/struct.GoyShell.html)
 
 #[macro_use]
 extern crate derive_new;
@@ -29,7 +10,6 @@ extern crate num_traits;
 
 pub mod adaptor;
 pub mod explicit;
-pub mod jacobian;
 pub mod lyapunov;
 pub mod ode;
 pub mod pde;
