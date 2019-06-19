c1f53d7ce8bf450d799544f382f4980e75821679
Toshiki Teramura <toshiki.teramura@gmail.com>
2019-06-19 15:02:10 +0900

    Add example for SWE

---
+++ examples/swe.rs
@@ -0,0 +1,34 @@
+extern crate eom;
+extern crate ndarray;
+extern crate ndarray_linalg;
+
+use eom::traits::*;
+use eom::*;
+use ndarray::*;
+use ndarray_linalg::*;
+
+fn main() {
+    let n = 128;
+    let l = 100.0;
+    let dt = 1e-3;
+    let interval = 100;
+    let step = 100;
+
+    let eom = pde::SWE::new(n, l, 0.1, 1.0);
+    let mut pair = pde::Pair::new(n);
+    let n_coef = eom.model_size();
+    let teo = semi_implicit::DiagRK4::new(eom, dt);
+    let mut teo = adaptor::nstep(teo, interval);
+
+    let x: Array1<c64> = c64::new(0.01, 0.0) * random(n_coef);
+    let x = adaptor::iterate(&mut teo, x, 100);
+
+    let ts = adaptor::time_series(x, &mut teo);
+    for (t, v) in ts.take(step).enumerate() {
+        let time = dt * t as f64;
+        print!("{:e},", time);
+        let u = pair.to_r(v.as_slice().unwrap());
+        let nums: Vec<_> = u.iter().map(|x| format!("{:e}", x)).collect();
+        println!("{}", nums.join(","));
+    }
+}
